8cc9a094d588a6e1f663af844bcbd8988bc0d997
UendelPedra <uendel.jesus@gmail.com>
2020-08-23 21:37:49 -0300

    Criado atributo complexo filhos

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,11 +1,16 @@
 class Pessoa:
-    def __init__(self, nome=None, idade=38):
+    def __init__(self, *filhos, nome=None, idade=38):
         self.idade = idade
         self.nome = nome
+        self.filhos = list(filhos)
     def cumprimentar(self):
         return 'Olá'
 if __name__ == '__main__':
-    p = Pessoa('Uendel')
-    print(p.cumprimentar())
-    print(p.nome)
-    print(p.idade)
+    esther = Pessoa(nome='Esther')
+    marly = Pessoa(nome='Marly')
+    uendel = Pessoa(esther, marly, nome='uendel')
+    print(uendel.cumprimentar())
+    print(uendel.nome)
+    print(uendel.idade)
+    for filho in uendel.filhos:
+        print(filho.nome)
