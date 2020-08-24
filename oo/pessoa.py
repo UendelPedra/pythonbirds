dc9e2933d061e1814df2e10df3926523ae017242
UendelPedra <uendel.jesus@gmail.com>
2020-08-23 21:19:10 -0300

    Criado atributos de instancias nome e idade

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,6 +1,11 @@
 class Pessoa:
+    def __init__(self, nome=None, idade=38):
+        self.idade = idade
+        self.nome = nome
     def cumprimentar(self):
         return 'Olá'
 if __name__ == '__main__':
-    p = Pessoa()
+    p = Pessoa('Uendel')
     print(p.cumprimentar())
+    print(p.nome)
+    print(p.idade)
