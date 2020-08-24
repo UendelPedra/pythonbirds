b15c07b56afab9990b3a0b522337427fc5a73a91
UendelPedra <uendel.jesus@gmail.com>
2020-08-23 22:03:34 -0300

    Criado atributo de classe olhos

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,4 +1,5 @@
 class Pessoa:
+    olhos = 2
     def __init__(self, *filhos, nome=None, idade=38):
         self.idade = idade
         self.nome = nome
@@ -15,5 +16,10 @@ if __name__ == '__main__':
     for filho in uendel.filhos:
         print(filho.nome)
     del uendel.filhos
+    uendel.olhos = 1
     print(uendel.__dict__)
     print(esther.__dict__)
+    print(Pessoa.olhos)
+    print(uendel.olhos)
+    print(esther.olhos)
+    print(id(Pessoa.olhos), id(uendel.olhos), id(esther.olhos))
