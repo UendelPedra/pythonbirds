cf8824fbc961929c11210d19749c19d683ba9bd6
UendelPedra <uendel.jesus@gmail.com>
2020-08-30 23:26:32 -0300

    concluido a composição

--- oo/carro.py
+++ oo/carro.py
@@ -1,4 +1,3 @@
-#from oo import motor
 
 """
 Você deve criar uma classe carro que vai possuir
@@ -95,6 +94,28 @@ Exemplo:
     'Oeste'
 
 """
+class Carro:
+    def __init__(self, direcao, motor):
+        self.motor = motor
+        self.direcao = direcao
+    def calcular_velocidade(self):
+        return self.motor.velocidade
+
+    def acelerar(self):
+        self.motor.acelerar()
+
+    def frear(self):
+        self.motor.frear()
+
+    def calcular_direcao(self):
+        return self.direcao.valor
+
+    def girar_a_direita(self):
+        self.direcao.girar_a_direita()
+
+    def girar_a_esquerda(self):
+        self.direcao.girar_a_esquerda()
+
 NORTE = 'Norte'
 LESTE ='Leste'
 SUL = 'Sul'
--- oo/motor.py
+++
@@ -1,8 +0,0 @@
-class Motor:
-    def __init__(self):
-        self.velocidade = 0
-    def acelerar(self):
-        self.velocidade += 1
-    def frear(self):
-        self.velocidade -= 2
-        self.velocidade = max(0, self.velocidade)
